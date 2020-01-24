bdf9684a537bc64170a18d1eb77b3869fd2f1a04
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-24 22:26:52 +0100

    Added like dislike onclick function

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,16 @@
 import React from "react";
 const Quote = props => {
   const { quotes, quoteCount } = props;
+  const likeClicked = event => {
+    console.log("LikeClicked", event.target.id);
+  };
+  const dislikeClicked = event => {
+    console.log("DislikeClicked", event.target.id);
+  };
   return (
     <div>
       <div>
-        <h1>Quote Garden</h1>
+        <h1>Quotes</h1>
         <p>Total number of Quote: {quoteCount}</p>
       </div>
       <div>
@@ -13,7 +19,20 @@ const Quote = props => {
             return (
               <div id={quote._id} key={quote._id}>
                 <p>{quote.quoteText}</p>
-                <p>{quote.quoteAuthor}</p>
+                <p>By: {quote.quoteAuthor}</p>
+                <p>
+                  <i
+                    id={quote._id}
+                    className="far fa-thumbs-up"
+                    onClick={likeClicked}
+                  ></i>
+
+                  <i
+                    id={quote._id}
+                    className="far fa-thumbs-down"
+                    onClick={dislikeClicked}
+                  ></i>
+                </p>
               </div>
             );
           })
