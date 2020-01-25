18b24539abe6c192700f23b0dbd716c7d9cf1742
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-25 12:31:12 +0100

    change text color using react hooks

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,25 +3,25 @@ import "./Quote.css";
 const Quote = props => {
   const { quotes, quoteCount } = props;
   const [quoteList, setQuoteList] = useState(quotes);
+  //const [isLiked, setQuoteColor] = useState(false);
   if (!quoteList) {
     setQuoteList([...quotes]);
   }
-  console.log("quotes", quotes);
-  console.log("quote list", quoteList);
+  //console.log("quotes", quotes);
+  //console.log("quote list", quoteList);
 
-  const [isLiked, setQuoteColor] = useState(false);
-  const likeClicked = event => {
-    console.log("LikeClicked", event.target.id);
-    document.getElementById(event.target.id).style.color = "green";
-    //setQuoteColor("green");
-    props.increaseTotalLike();
-  };
-  const dislikeClicked = event => {
-    console.log("DislikeClicked", event);
-    document.getElementById(event.target.id).style.color = "red";
-    // setQuoteColor("red");
-    props.increaseTotalDislike();
-  };
+  // const likeClicked = event => {
+  //   console.log("LikeClicked", event.target.id);
+  //   document.getElementById(event.target.id).style.color = "green";
+  //   //setQuoteColor("green");
+  //   props.increaseTotalLike();
+  // };
+  // const dislikeClicked = event => {
+  //   console.log("DislikeClicked", event);
+  //   document.getElementById(event.target.id).style.color = "red";
+  //   // setQuoteColor("red");
+  //   props.increaseTotalDislike();
+  // };
   return (
     <div>
       <div>
@@ -32,9 +32,35 @@ const Quote = props => {
         {console.log("quote list", quoteList)}
         {quoteList ? (
           quoteList.map(quote => {
+            const likeClicked = event => {
+              //console.log("LikeClicked", event.target.id);
+              const updatedQuote = quoteList.map(quote => {
+                return quote._id === event.target.id
+                  ? { ...quote, style: "green" }
+                  : quote;
+              });
+              //console.log("Updated quote", updatedQuote);
+              props.increaseTotalLike();
+              setQuoteList(updatedQuote);
+            };
+            const dislikeClicked = event => {
+              const updatedQuote = quoteList.map(quote => {
+                return quote._id === event.target.id
+                  ? { ...quote, style: "red" }
+                  : quote;
+              });
+              console.log("Updated quote", updatedQuote);
+              props.increaseTotalDislike();
+              setQuoteList(updatedQuote);
+            };
             return (
               <div>
-                <div style={{ color: "black" }} id={quote._id} key={quote._id}>
+                <div
+                  style={{ color: quote.style ? quote.style : "black" }}
+                  testtag={quote.style}
+                  id={quote._id}
+                  key={quote._id}
+                >
                   <p>{quote.quoteText}</p>
                   <p>By: {quote.quoteAuthor}</p>
                 </div>
